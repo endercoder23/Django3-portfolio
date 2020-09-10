a25e512213ccfde92de8e3810376f9d2e07cf882
ender <drj2311@gmail.com>
2020-09-10 13:54:18 +0530

    new local settings

--- portfoliooo/settings.py
+++ portfoliooo/settings.py
@@ -124,6 +124,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = BASE_DIR / 'media'
 
+try:
+    from .local_settings import *
+except ImportError:
+    print("looks like no local file, You want is production")
